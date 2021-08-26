ed70e12126834ed6c9064494e2a5ae991c5d305b
邓如春 <1107151332@qq.com>
2021-08-26 17:53:12 +0800

    关联 https://codesandbox.io/

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-require(['dojo/parser', 'dojox/mvc/getStateful'], function (parser, getStateful) {
-    var stateful =window.stateful= getStateful({
-        value: "",
-        children: [],
-        onClick: function () {
-            this.children.push({ text: this.value });
-            this.set("value", "")
-        }
-    });
-    
-    parser.parse({ propsThis: stateful, noStart: false, template: true });
-})
+/* eslint-disable import/no-amd*/
+require(["dojo/parser", "dojox/mvc/getStateful"], function (
+  parser,
+  getStateful
+) {
+  var stateful = getStateful({
+    value: "",
+    children: [],
+    onClick: function () {
+      this.children.push({ text: this.value });
+      this.set("value", "");
+    }
+  });
+
+  parser.parse({ propsThis: stateful, noStart: false, template: true });
+});
